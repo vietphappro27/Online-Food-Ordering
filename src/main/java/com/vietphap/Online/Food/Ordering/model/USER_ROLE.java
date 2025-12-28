d67f3612070962814e038bff2d81e247de7f37db
vietphap27 <phaplon32@gmail.com>
2025-12-28 16:48:23 +0700

    USER model

---
+++ src/main/java/com/vietphap/Online/Food/Ordering/model/USER_ROLE.java
@@ -0,0 +1,9 @@
+package com.vietphap.Online.Food.Ordering.model;
+
+public enum USER_ROLE {
+    ROLE_CUSTOMER,
+
+    ROLE_CUSTOMER_OWNER,
+
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/vietphap/Online/Food/Ordering/model/User.java
@@ -0,0 +1,45 @@
+package com.vietphap.Online.Food.Ordering.model;
+
+import java.util.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.vietphap.Online.Food.Ordering.dto.RestaurantDTO;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String fullname;
+
+    private String email;
+
+    private String password;
+
+    private USER_ROLE role;
+
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "customer")
+    private List<Order> orders = new ArrayList<>();
+
+    @ElementCollection
+    private List<RestaurantDTO> favorites = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Address> addresses = new ArrayList<>();
+}
