ed5dfbdbb16c7604c257148238015bd05ad95ef7
vietphap27 <phaplon32@gmail.com>
2025-12-28 17:10:23 +0700

    Order  model

---
+++ src/main/java/com/vietphap/Online/Food/Ordering/model/Order.java
@@ -0,0 +1,52 @@
+package com.vietphap.Online.Food.Ordering.model;
+
+import java.util.Date;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "orders")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private User customer;
+
+    @ManyToOne
+    @JsonIgnore
+    private Restaurant restaurant;
+
+    private Long totalAmount;
+
+    private String OrderStatus;
+
+    private Date createdAt;
+
+    @ManyToOne
+    private Address deliveryAddress;
+
+    @OneToMany
+    private List<OrderItem> items;
+
+    private int totalItems;
+
+    private int totalPrice;
+}
---
+++ src/main/java/com/vietphap/Online/Food/Ordering/model/OrderItem.java
@@ -0,0 +1,30 @@
+package com.vietphap.Online.Food.Ordering.model;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    private Food food;
+
+    private int quantity;
+    private long totalPrice;
+
+    private List<String> ingredients;
+}
